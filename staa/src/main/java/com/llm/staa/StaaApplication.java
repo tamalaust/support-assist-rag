5b8d0703a13835c16f2b88a7abdea9a0e5a0fdd0
Md Zahidul Islam <mdzahid.islam@outlook.com>
2026-08-11 17:28:53 +0600

    inital commit

---
+++ staa/src/main/java/com/llm/staa/StaaApplication.java
@@ -0,0 +1,13 @@
+package com.llm.staa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StaaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StaaApplication.class, args);
+	}
+
+}
---
+++ staa/src/test/java/com/llm/staa/StaaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.llm.staa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StaaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
