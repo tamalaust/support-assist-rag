b95472b0f5ec3162a23e1e049937079761bfdd8c
Md Zahidul Islam <mdzahid.islam@outlook.com>
2026-08-12 12:35:32 +0600

    basic chat api using ollama (llama3.2)

---
+++ staa/src/main/java/com/llm/staa/chat/ChatController.java
@@ -0,0 +1,27 @@
+package com.llm.staa.chat;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class ChatController {
+    private final ChatClient chatClient;
+
+    public ChatController(ChatClient.Builder chatClientBuilder) {
+        this.chatClient = chatClientBuilder.build();
+    }
+
+    @GetMapping("/chat")
+    public String chat(@RequestParam String curiosity) {
+        return chatClient.prompt()
+                .user(curiosity)
+                .call()
+                .content();
+    }
+
+}
